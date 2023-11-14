9f1d35ab87ad50f1c28615092037c14d4491ad8f
leeho7029 <ekghdidi@naver.com>
2023-11-14 14:56:03 +0900

    게시글 insert만 됨..지도컬럼 추가해야됨..

--- team35.sql
+++ team35.sql
@@ -38,3 +38,50 @@ CREATE VIEW userList AS(
 	FROM user u
 	LEFT JOIN role r ON u.role_id = r.role_id
 );
+
+CREATE TABLE book(
+	book_no INT PRIMARY KEY AUTO_INCREMENT,
+	req_no INT,
+	login_id VARCHAR(255) NOT null,
+	isbn VARCHAR(100) NOT NULL,
+	image VARCHAR(100) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	author VARCHAR(50) NOT NULL,
+	publisher VARCHAR(50) NOT NULL,
+	pubdate VARCHAR(30) NOT null 
+);
+
+CREATE TABLE market(
+	 market_no INT AUTO_INCREMENT PRIMARY KEY,	-- 상품 번호 	
+    title VARCHAR(100) NOT NULL,	-- 제목
+    price int NOT NULL,		-- 가격
+    content VARCHAR(5000) NOT null,	-- 설명
+	 login_id VARCHAR(255) NOT null,	-- 작성자 id
+    active int NOT NULL DEFAULT 0,	-- 거래 상태(거래 완료 여부)
+    conditions int NOT NULL,	-- 상품 상태(최상 상 중 하)
+    regdate DATETIME DEFAULT CURRENT_TIMESTAMP	-- 등록일
+);
+
+CREATE TABLE request(
+	 req_no INT AUTO_INCREMENT PRIMARY KEY,	-- 상품 번호 	
+    title VARCHAR(100) NOT NULL,	-- 제목
+    price int NOT NULL,		-- 가격
+    content VARCHAR(5000),	-- 설명
+	 login_id VARCHAR(255) NOT NULL,	-- 작성자 id
+    active varchar(20) NOT NULL,	-- 거래 상태(거래 완료 여부)
+    regdate DATETIME DEFAULT CURRENT_TIMESTAMP	-- 등록일
+);
+
+CREATE TABLE photos(
+  photo_no int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  market_no INT,
+  saveFolder VARCHAR(300) NOT NULL,
+  originFile VARCHAR(300) NOT NULL,
+  saveFile VARCHAR(300) NOT NULL
+);
+
+CREATE VIEW userList AS(
+	SELECT u.user_id AS user_id, u.active AS ACTIVE, u.login_id AS login_id, u.user_name AS user_name, u.password AS PASSWORD, u.role_id AS role_id, r.role AS roleNm
+	FROM user u
+	LEFT JOIN role r ON u.role_id = r.role_id
+);
