cdfe69b42089f679117316009364db5b360baab8
leeho7029 <ekghdidi@naver.com>
2023-11-15 14:55:55 +0900

    [231115 leeho] 삽니다 게시판 기능 완료..css 수정 필요

---
+++ src/main/java/com/tsherpa/team35/biz/RequestService.java
@@ -0,0 +1,44 @@
+package com.tsherpa.team35.biz;
+
+import com.tsherpa.team35.entity.Request;
+import com.tsherpa.team35.per.RequestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RequestService {
+    @Autowired
+    private RequestMapper requestMapper;
+
+
+
+    public void reqInsert(Request request) throws Exception{
+        requestMapper.requestInsert(request);
+    }
+    public List<Request> requestList() throws Exception{
+        return requestMapper.requestList();
+    }
+    public Request requestDetail(int reqNo) throws Exception{
+        return requestMapper.requestDetail(reqNo);
+    }
+    public List<Request> getMoreRequests(int offset, int limit) {
+        return requestMapper.getMoreRequests(offset, limit);
+    }
+    public void requestEdit(Request request) throws Exception {
+        requestMapper.requestEdit(request);
+    }
+
+    public void requestDelete(int reqNo) throws Exception{
+        requestMapper.requestDelete(reqNo);
+    }
+
+    public List<Request> allRequest() throws Exception{
+        return requestMapper.allRequest();
+    }
+
+    public void requestEditAll(Request request) throws Exception {
+        requestMapper.requestEditAll(request);
+    }
+}
