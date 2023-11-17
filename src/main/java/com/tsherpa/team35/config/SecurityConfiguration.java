30dab1f9296cc95463cf18d1d0a9644e1bf4adf5
soumunda8 <soumunda8@naver.com>
2023-11-17 17:10:46 +0900

    채팅 진행중\

--- src/main/java/com/tsherpa/team35/config/SecurityConfiguration.java
+++ src/main/java/com/tsherpa/team35/config/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN") // ADMIN 권한의 유저만 /home 에 접근가능
-                .antMatchers("/mypage","/pwEdit","/userEdit").hasRole("USER")
+                .antMatchers("/mypage","/pwEdit","/userEdit").hasAnyRole("USER", "TEACHER")
                 .antMatchers("/","/**","/login","/join").permitAll()
                 .mvcMatchers("/css/**","/js/**","/image/**","/fonts/**","/clEditor/**", "/lib/**").permitAll()
                 .anyRequest().authenticated()
